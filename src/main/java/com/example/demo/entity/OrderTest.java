a2661e83fcd338b9350eee7ba9bd56082c25c153
GWAKMW\user <gwakmw@duse.co.kr>
2022-12-28 22:24:26 +0900

    고아객체 제거 테스트

--- src/main/java/com/example/demo/entity/OrderTest.java
+++ src/main/java/com/example/demo/entity/OrderTest.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import com.example.demo.constant.ItemSellStatus;
 import com.example.demo.repository.ItemRepository;
+import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -67,6 +68,39 @@ public class OrderTest {
         Order savedOrder = orderRepository.findById(order.getId())
                 .orElseThrow(EntityNotFoundException::new);
         assertEquals(3, savedOrder.getOrderItems().size());
+    }
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public Order createOrder() {
+        Order order = new Order();
+
+        for (int i = 0; i < 3; i++) {
+            Item item = createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+
+        Member member = new Member();
+        memberRepository.save(member);
+
+        order.setMember(member);
+        orderRepository.save(order);
+        return order;
+    }
+
+    @Test
+    @DisplayName("고아객체 제거 테스트")
+    public void orphanRemovalTest() {
+        Order order = this.createOrder();
+        order.getOrderItems().remove(0);
+        em.flush();
     }
 
 }
