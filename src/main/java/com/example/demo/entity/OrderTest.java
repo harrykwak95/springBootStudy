1b371d6be2e647e2f1072dde4fae256dec597776
GWAKMW\user <gwakmw@duse.co.kr>
2022-12-29 20:06:57 +0900

    LAZY LOADING 처리 및 테스트

--- src/main/java/com/example/demo/entity/OrderTest.java
+++ src/main/java/com/example/demo/entity/OrderTest.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import com.example.demo.constant.ItemSellStatus;
 import com.example.demo.repository.ItemRepository;
 import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.OrderItemRepository;
 import com.example.demo.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -101,6 +102,25 @@ public class OrderTest {
         Order order = this.createOrder();
         order.getOrderItems().remove(0);
         em.flush();
+    }
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    public void lazyLoadingTEST() {
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        System.out.println("Order class : " + orderItem.getOrder().getClass());
+        System.out.println("===============================================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("===============================================");
     }
 
 }
