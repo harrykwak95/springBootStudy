78610e395b6d29fa7521f1aee9fd44032b6867d7
GWAKMW\user <gwakmw@duse.co.kr>
2023-01-18 00:59:39 +0900

    CREATE : 이미지 파일 업로드 과정까지 진행

---
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.ItemFormDto;
+import com.example.demo.entity.Item;
+import com.example.demo.entity.ItemImg;
+import com.example.demo.repository.ItemImgRepository;
+import com.example.demo.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+            if(i == 0)
+                itemImg.setRepimgYn("Y");
+            else
+                itemImg.setRepimgYn("N");
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+        return item.getId();
+    }
+
+}
