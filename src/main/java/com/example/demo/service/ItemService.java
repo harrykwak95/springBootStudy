149bf49ac5bcc4b0364fa2fa02a77c92971e65e8
GWAKMW\user <gwakmw@duse.co.kr>
2023-01-18 23:21:24 +0900

    CREATE : 수정을 위한 작업

--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -1,15 +1,18 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.ItemFormDto;
+import com.example.demo.dto.ItemImgDto;
 import com.example.demo.entity.Item;
 import com.example.demo.entity.ItemImg;
 import com.example.demo.repository.ItemImgRepository;
 import com.example.demo.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -37,6 +40,25 @@ public class ItemService {
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
         }
         return item.getId();
+    }
+
+
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId){
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
     }
 
 }
